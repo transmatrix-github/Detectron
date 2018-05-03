1de2032385ccefaf61a7f03a03faf1965ad63b71
Wen Li <liwenbnu@gmail.com>
2018-05-03 20:22:53 +0800

    create scripts folder

--- run_demo_training.sh
+++
@@ -1,3 +0,0 @@
-python2 tools/train_net.py \
-    --cfg configs/exps/coco2017_1gpu_e2e_faster_rcnn_R-50-FPN.yaml \
-    OUTPUT_DIR ./detectron-output
--- run_train_fast_rcnn_r101.sh
+++
@@ -1,3 +0,0 @@
-python2 tools/train_net.py \
-    --cfg configs/exps/coco2017_e2e_faster_rcnn_R-101-FPN_1x.yaml \
-    OUTPUT_DIR ./detectron-output/fast_rcnn_r101

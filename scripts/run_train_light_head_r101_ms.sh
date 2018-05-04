84a2c424a21a4c3818df897c30bd1bfa4aebc145
Wen Li <liwenbnu@gmail.com>
2018-05-05 05:28:37 +0800

    revised training scripts

--- scripts/run_train_light_head_r101_ms.sh
+++ scripts/run_train_light_head_r101_ms.sh
@@ -1,8 +1,9 @@
 CONFIG=coco2017_e2e_light_rcnn_R-101-C4_ms_1x.yaml
-OUTPUT_DIR=./detectron-output/$CONFIG
+DETECTRON=/home/amax/workspace/Detectron
+OUTPUT_DIR=$DETECTRON/detectron-output/$CONFIG
 mkdir -p $OUTPUT_DIR
 # DATE=$(date +'%F_%T')
-nohup python2 tools/train_net.py \
-    --cfg configs/exps/$CONFIG \
+nohup python2 $DETECTRON/tools/train_net.py \
+    --cfg $DETECTRON/configs/exps/$CONFIG \
     OUTPUT_DIR $OUTPUT_DIR \
-    > ./detectron-output/$CONFIG/train_log.txt &
+    > $OUTPUT_DIR/train_log.txt &

6d04fc1f0e65f16350461eedfb411359d0d0ab4c
WenLi <wen@WenLis-MacBook-Pro.local>
2018-05-07 23:27:04 +0200

    add light head to fast rcnn heads

---
+++ scripts/run_train_light_head_r101_fpn.sh
@@ -0,0 +1,9 @@
+CONFIG=coco2017_e2e_light_rcnn_R-101-FPN_1x.yaml
+DETECTRON=/home/amax/workspace/Detectron
+OUTPUT_DIR=$DETECTRON/detectron-output/$CONFIG
+mkdir -p $OUTPUT_DIR
+# DATE=$(date +'%F_%T')
+nohup python2 $DETECTRON/tools/train_net.py \
+    --cfg $DETECTRON/configs/exps/$CONFIG \
+    OUTPUT_DIR $OUTPUT_DIR \
+    > $OUTPUT_DIR/train_log.txt &

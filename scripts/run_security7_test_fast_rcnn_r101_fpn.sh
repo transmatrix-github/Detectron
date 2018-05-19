12920cc006d6c055de4155dfe97e08e0475636d5
liwen <liwenbnu@gmail.com>
2018-05-19 20:21:55 +0200

    updated scripts

--- scripts/run_security7_test_fast_rcnn_r101_fpn.sh
+++ scripts/run_security7_test_fast_rcnn_r101_fpn.sh
@@ -7,4 +7,4 @@ mkdir -p $OUTPUT_DIR
 nohup python2 $DETECTRON/tools/test_net.py \
     --cfg $DETECTRON/configs/exps/$CONFIG \
     TEST.WEIGHTS ./detectron-output/$CONFIG/train/security_train_alg_synthesis\:security_train_auto_synthesis:security_train_dongle_synthesis\:security_train_pro_synthesis\:security_train_real_package\:security_train_real_polyhedron\:security_train_single_contraband/generalized_rcnn/model_final.pkl 
-    OUTPUT_DIR $OUTPUT_DIR/test
+    OUTPUT_DIR $OUTPUT_DIR/test > test_log.txt

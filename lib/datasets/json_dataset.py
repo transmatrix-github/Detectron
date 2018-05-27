010d20ec625d52dc1159f5ecdda84d1702e3cfec
WenLi <wen@WenLis-MacBook-Pro.local>
2018-05-27 15:10:06 +0200

    updated scripts

--- lib/datasets/json_dataset.py
+++ lib/datasets/json_dataset.py
@@ -137,6 +137,7 @@ class JsonDataset(object):
         im_path = os.path.join(
             self.image_directory, self.image_prefix + entry['file_name']
         )
+        print(im_path)
         assert os.path.exists(im_path), 'Image \'{}\' not found'.format(im_path)
         entry['image'] = im_path
         entry['flipped'] = False

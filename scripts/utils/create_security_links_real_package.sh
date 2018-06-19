f064e0627a116ea8851e77224fdd3259ea7ea289
Wen Li <liwenbnu@gmail.com>
2018-06-19 17:34:05 +0200

    updated scripts

--- scripts/utils/create_security_links_real_package.sh
+++ scripts/utils/create_security_links_real_package.sh
@@ -4,10 +4,10 @@ train_list="20180114 20180126 20180403 20180404 20180416 20180417 20180418 20180
 test_list="20180507 20180515 20180525 20180531"
 mkdir -p $DATA_ROOT/Real-Package_train
 mkdir -p $DATA_ROOT/Real-Package_val
-for f in train_list;
+for f in ${train_list};
   do ln -sn $DATA_ROOT/Real-Package/${f} $DATA_ROOT/Real-Package_train/${f}; 
 done;
 
-for f in test_list;
+for f in ${test_list};
   do ln -sn $DATA_ROOT/Real-Package/${f} $DATA_ROOT/Real-Package_val/${f}; 
 done;

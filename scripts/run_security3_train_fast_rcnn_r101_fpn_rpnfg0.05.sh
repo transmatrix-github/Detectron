b2f030259e1d0b90beaea1ead8f2627e868cf128
Wen Li <liwenbnu@gmail.com>
2018-06-19 17:10:44 +0200

    added evaluation scripts

---
+++ scripts/run_security3_train_fast_rcnn_r101_fpn_rpnfg0.05.sh
@@ -0,0 +1,9 @@
+CONFIG=security3_e2e_faster_rcnn_R-101-FPN_rpnfg0.05_1x.yaml
+DETECTRON=/home/amax/workspace/Detectron
+OUTPUT_DIR=$DETECTRON/detectron-output/$CONFIG
+mkdir -p $OUTPUT_DIR
+# DATE=$(date +'%F_%T')
+nohup python2 $DETECTRON/tools/train_net.py \
+    --cfg $DETECTRON/configs/exps/$CONFIG \
+    OUTPUT_DIR $OUTPUT_DIR \
+    > $OUTPUT_DIR/train_log.txt &

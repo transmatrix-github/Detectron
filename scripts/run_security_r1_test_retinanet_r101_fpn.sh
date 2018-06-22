951652c9a7de60e9b97553c98354c561701ccce8
Wen Li <liwenbnu@gmail.com>
2018-06-22 18:20:13 +0200

    updated scripts

--- scripts/run_security_r1_test_retinanet_r101_fpn.sh
+++ scripts/run_security_r1_test_retinanet_r101_fpn.sh
@@ -6,5 +6,4 @@ mkdir -p $OUTPUT_DIR
 nohup python2 $DETECTRON/tools/test_net.py \
     --cfg $DETECTRON/configs/exps/$CONFIG \
     TEST.WEIGHTS ./detectron-output/$CONFIG/train/security_train_real_package_train/retinanet/model_final.pkl \    
-    OUTPUT_DIR $OUTPUT_DIR/test \
-    > $OUTPUT_DIR/test_log.txt &
+    OUTPUT_DIR $OUTPUT_DIR/test > $OUTPUT_DIR/test_log.txt &
